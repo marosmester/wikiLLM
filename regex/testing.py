761d020ba9e30b895bffd788f100e1fc83eff9f4
marosmester <mestermaros@gmail.com>
2025-02-24 22:16:12 +0100

    regex year search added

--- regex/testing.py
+++ regex/testing.py
@@ -1,3 +1,15 @@
 import re
-text = "Elizabeth (far left) on the balcony of Buckingham Palace with her family and Winston Churchill, 8 May 1945"
 
+text = "Elizabeth (far left) on the balcony of Buckingham Palace with her family and Winston Churchill, 8 May 1945"
+text += " and her 3046 soldiers born in 982"
+print(text)
+
+# patterns
+p1 = r"\d{4}"                       # 4 digit number
+p2 = r"\b[12]\d{3}\b"               # 4 digit number beginning with a 1 or a 2
+p3 = r"\b([12]\d{3}|\d{3})\b"       # 4 digit number beginning with a 1 or a 2, or, any 3 digit number
+
+x1 = re.search(p3, text)
+x2 = re.findall(p3, text)
+print(x1)
+print(x2)
