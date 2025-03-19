a61d7437f40590e59b8c9f5ba4b9db159d6b2b7c
marosmester <mestermaros@gmail.com>
2025-03-19 19:19:01 +0100

    helper functions update

---
+++ regex/helper_function.py
@@ -0,0 +1,32 @@
+import re
+import json
+import os
+from pathlib import Path
+import numpy as np
+
+# Maros 
+
+def find_birth_year(path_to_person):
+    '''
+    Finds the person's birth year in the wikipedia body text file.  
+    '''
+    # Get birth year from wiki main text
+    main_text = path_to_person / "text.txt"
+    pattern = r"Category:\b[12]\d{3}\b births" # 4 digit number beginning with a 1 or a 2
+    x = find_year_file(main_text, pattern)
+    if x is not None:
+        byear = int( x.group()[9:13] )           # extract birth yeat as int
+    else:
+        byear = None
+
+    return byear
+
+def find_year_file(fpath, pattern):
+    '''
+    Search any elligible file AS IT WAS A TXT FILE.
+    '''
+    #print(fpath)
+    with open(fpath, "r") as f:
+        content = f.read()
+    match = re.search(pattern, content)
+    return match
